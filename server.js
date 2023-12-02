0cae3461e79e9a61bfad71f086cd895285108e0f
NDWnatural <134720252+NDWnatural@users.noreply.github.com>
2023-12-02 18:05:51 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ytdl = require('ytdl-core');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/download', async (req, res) => {
+  try {
+    const videoUrl = req.body.videoUrl;
+    const info = await ytdl.getInfo(videoUrl);
+    const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio' });
+
+    res.header('Content-Disposition', `attachment; filename="${info.title}.mp3"`);
+    ytdl(videoUrl, { format: audioFormat })
+      .pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao processar o download.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
