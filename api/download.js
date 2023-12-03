ec39425e41a83404543c5b315828f80b94f6cec2
NDWnatural <134720252+NDWnatural@users.noreply.github.com>
2023-12-03 07:50:33 -0300

    Add files via upload

---
+++ api/download.js
@@ -0,0 +1,21 @@
+// api/download.js
+const { send } = require('micro');
+const { parse } = require('url');
+const { createServer } = require('http');
+const cors = require('micro-cors')();
+
+const handler = async (req, res) => {
+  const { pathname, query } = parse(req.url, true);
+  if (pathname === '/download') {
+    // Lógica de download aqui
+    send(res, 200, 'Download pronto!');
+  } else {
+    send(res, 404, 'Rota não encontrada');
+  }
+};
+
+const server = createServer(cors(handler));
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Servidor Vercel Function rodando!');
+});
