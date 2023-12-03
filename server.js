fcb4de2b5489c2932a2ebd610d6d2bcff742edab
NDWnatural <134720252+NDWnatural@users.noreply.github.com>
2023-12-03 19:24:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ndwnatural.github.io');
+  res.header('Access-Control-Allow-Origin', 'https://ndwnatural.github.io');
   next();
 });
 app.use(bodyParser.urlencoded({ extended: true }));
