a49cf72cb6803f66199e08eb1e44f8b182ccd616
NDWnatural <134720252+NDWnatural@users.noreply.github.com>
2023-12-03 22:48:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+function download() {
+    const apiUrl = 'https://abobus-snowy.vercel.app';
+    const videoUrl = document.getElementById('videoUrl').value;
 
+    if (!videoUrl) {
+        alert('Por favor, insira a URL do vídeo.');
+        return;
+    }
+
+    fetch(apiUrl, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        headers: {
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
+        },
+        body: `videoUrl=${encodeURIComponent(videoUrl)}`
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao iniciar o download.');
+        }
+        return response.blob();
+    })
+    .then(blob => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'audio.mp3';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    })
+    .catch(error => {
+        console.error(error);
+        alert('Erro ao processar o download.');
+    });
+}
