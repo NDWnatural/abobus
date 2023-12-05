e9bf9338691a7e476caed51025cd6ac90260db79
NDWnatural <134720252+NDWnatural@users.noreply.github.com>
2023-12-05 03:57:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 function download() {
-  const apiUrl = 'https://abobus-snowy.vercel.app';
-  const videoUrl = document.getElementById('videoUrl').value;
+    const apiUrl = 'https://abobus-snowy.vercel.app/';
+    const videoUrl = document.getElementById('videoUrl').value;
 
-  if (!videoUrl) {
-      alert('Por favor, insira a URL do vídeo.');
-      return;
-  }
+    if (!videoUrl) {
+        alert('Por favor, insira a URL do vídeo.');
+        return;
+    }
 
-  fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ videoUrl }),
-  })
-
+    // Configurando a solicitação OPTIONS
+    fetch(apiUrl, {
+        method: 'OPTIONS',
+    })
     .then(response => {
         if (!response.ok) {
-            throw new Error('Erro ao iniciar o download.');
+            throw new Error('Erro ao iniciar o download. (OPTIONS)');
+        }
+        // Continuar com a solicitação POST
+        return fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ videoUrl }),
+        });
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao iniciar o download. (POST)');
         }
         return response.blob();
     })
@@ -32,47 +41,7 @@ function download() {
         window.URL.revokeObjectURL(url);
     })
     .catch(error => {
-    console.error(error);
-    alert('Erro ao processar o download.');
-  });
-}
-//segundo java//
-
-    var header           = document.getElementById('header');
-    var navigationHeader = document.getElementById('navigation_header');
-    var content          = document.getElementById('content');
-    var showSidebar      = false;
-
-    function toggleSidebar()
-    {
-        showSidebar = !showSidebar;
-        if(showSidebar)
-        {
-            navigationHeader.style.marginLeft = '-10vw';
-            navigationHeader.style.animationName = 'showSidebar';
-            content.style.filter = 'blur(2px)';
-        }
-        else
-        {
-            navigationHeader.style.marginLeft = '-100vw';
-            navigationHeader.style.animationName = '';
-            content.style.filter = '';
-        }
-    }
-
-    function closeSidebar()
-    {
-        if(showSidebar)
-        {
-            showSidebar = true;
-            toggleSidebar();
-        }
-    }
-
-    window.addEventListener('resize', function(event) {
-        if(window.innerWidth > 768 && showSidebar) 
-        {  
-            showSidebar = true;
-            toggleSidebar();
-        }
+        console.error(error);
+        alert('Erro ao processar o download.');
     });
+}
